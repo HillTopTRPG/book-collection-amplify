65235858661ac9370188d27e4ca764e4b84b1e5f
HillTopTRPG <mihikari90@gmail.com>
2025-09-19 06:30:32 +0900

    Change 書籍情報の項目表示制御

--- src/components/Card/NdlCard/index.tsx
+++ src/components/Card/NdlCard/index.tsx
@@ -1,30 +1,37 @@
 import BookImage from '@/components/BookImage.tsx';
 import CardFrame from '@/components/Card/CardFrame.tsx';
 import NdlCardItem from '@/components/Card/NdlCard/NdlCardItem.tsx';
+import type { NdlFullOptions } from '@/components/Drawer/BookDetailDrawer/NdlOptionsForm.tsx';
 import type { BookData } from '@/types/book.ts';
-import type { NdlOptions } from '@/utils/fetch.ts';
 
 type Props = {
   ndl: BookData;
-  options: NdlOptions | null | undefined;
+  options: NdlFullOptions | null | undefined;
   anywhere?: string;
 };
 
 export default function NdlCard({ ndl, options, anywhere }: Props) {
   const isbn = ndl.isbn?.replaceAll('-', '');
 
+  const isViewTitle = options?.title !== ndl.title;
+  const creatorText = ndl.creator?.join(', ') ?? '';
+  const isViewCreator = !options?.useCreator && options?.creator !== creatorText;
+  const isViewPublisher = !options?.usePublisher && options?.publisher !== ndl.publisher;
+
   return (
     <CardFrame className="items-start">
       <BookImage isbn={isbn} defaultUrl={null} />
       <div className="flex items-baseline flex-wrap gap-x-3 flex-1">
         <div className="w-full flex items-baseline flex-wrap gap-x-3">
-          <NdlCardItem value={ndl.title} highLight={anywhere} className="text-lg font-bold inline-block" />
+          { isViewTitle ? (
+            <NdlCardItem value={ndl.title} highLight={anywhere} className="text-lg font-bold inline-block" />
+          ) : null }
           <NdlCardItem value={ndl.volume} highLight={anywhere} className="text-lg font-bold" />
           <NdlCardItem value={ndl.volumeTitle} highLight={anywhere} className="text-lg font-bold" />
         </div>
         <div className="w-full flex items-baseline flex-wrap gap-x-3">
-          { options?.creator ? null : <NdlCardItem value={ndl.creator?.join(', ')} highLight={anywhere} className="text-xs" /> }
-          { options?.publisher ? null : <NdlCardItem value={ndl.publisher} highLight={anywhere} className="text-xs" /> }
+          { isViewCreator ? <NdlCardItem value={creatorText} highLight={anywhere} className="text-xs" /> : null }
+          { isViewPublisher ? <NdlCardItem value={ndl.publisher} highLight={anywhere} className="text-xs" /> : null }
         </div>
         <NdlCardItem value={ndl.edition} highLight={anywhere} className="text-xs" />
         <NdlCardItem value={ndl.date} highLight={anywhere} label="発売日" className="text-xs" />
